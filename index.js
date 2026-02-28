996269fefbd7d8ae70e083b1aa7d4e67657a537b
yashwanthN24 <yashwanthnadddana2002@gmail.com>
2026-02-28 13:41:10 +0530

    Second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 3000
 
 app.get('/' , (req , res) =>{
-    res.json({ msg : "Hello from server "});
+    res.json({ msg : "Hello from server from v1 "});
 })
 
 app.listen(PORT , ()=> {
