c72716b68b70cdb26b6633b80b17860eac30e3d6
yashwanthN24 <yashwanthnadddana2002@gmail.com>
2026-02-28 14:02:30 +0530

    Third commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-const PORT = process.env.PORT ?? 3000
+const app = express();
+const PORT = process.env.PORT ?? 3000;
 
-app.get('/' , (req , res) =>{
-    res.json({ msg : "Hello from server from v1 "});
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello from server from v1 and v2 " });
+});
 
-app.listen(PORT , ()=> {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
