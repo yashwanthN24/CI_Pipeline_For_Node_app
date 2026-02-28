76eb4822bac983104b6566eec1b69c60d6b2a2c2
yashwanthN24 <yashwanthnadddana2002@gmail.com>
2026-02-28 11:40:21 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT ?? 3000
+
+app.get('/' , (req , res) =>{
+    res.json({ msg : "Hello from server "});
+})
+
+app.listen(PORT , ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
